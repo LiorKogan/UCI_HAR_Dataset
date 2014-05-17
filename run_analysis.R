bf8436c13b88fa9a18d6fc7712811064c5ec7339
LiorKogan <koganlior1@gmail.com>
2014-05-17 17:28:37 +0300

    Update

--- run_analysis.R
+++ run_analysis.R
@@ -20,7 +20,8 @@ message("reading subject labels for the test data set")
 train_subj_labels <- read.table("train//subject_train.txt")
 
 message("read activity labels descriptive names")
-act_label_names <- read.table("activity_labels.txt")[,2]
+act_label_table <- read.table("activity_labels.txt")
+act_label_names <- factor(act_label_table[,1], labels=act_label_table[,2])
 
 message("read feature names")
 features <- read.table("features.txt")[,2]
@@ -48,17 +49,18 @@ mean_std_cols <- sort(c(mean_cols,std_cols))
 # extract only mean and stddevs columns
 means_stds <- combined[,mean_std_cols]
 
-# add first row with activity label descriptive name
+# add first col with activity label descriptive name
 means_stds <- cbind(act_label_names[combined_act_labels], means_stds)
+colnames(means_stds)[1] <- "activity"
 
 # ================================================
 message("calculating act_subj_avg")
 
 # create act_subj_avg: 
-# average of each sum/mean variable (1..87) for each activity (1..6) and each subject (1..30)
+# average of each sum/mean variable (1..86) for each activity (1..6) and each subject (1..30)
 # column 1     : activity (val: 1.. 6)
 # column 2     : subject  (val: 1..30)
-# columns 3..89: average of each of the 87 mean/std variables
+# columns 3..88: average of each of the 86 mean/std variables
 # total rows: subjects * activities = 180
 
 no_subjects <- length(unique(combined_subj_labels)) # no. of subjects
@@ -78,29 +80,26 @@ for (r in 1:nrow(means_stds)) # for each test and train case
 	}
 }
 
-# add leading columns: activity, subject
-
 act_subj_avg = sums / counts
-colnames(act_subj_avg)= rep("", times= ncol(act_subj_avg)) # set empty column names
-for (v in 1: (ncol(means_stds)-1)) # for each mean/std variable
-	colnames(act_subj_avg)[v] = paste("mean(", colnames(means_stds)[v+1], ")", sep="") # set column name
 
-act_subj_avg<- cbind(0, cbind(0, act_subj_avg)) # insert two leading columns
-colnames(act_subj_avg)[1] <- "activity"
-colnames(act_subj_avg)[2] <- "subject"
+act_subj_avg<- data.frame(act_subj_avg)                   # convert matrix to data frame
+act_subj_avg<- cbind(act_label_names[1], 0, act_subj_avg) # insert two leading columns: activity, subject
+
+colnames(act_subj_avg)[1:2] <- c("activity", "subject")   # set names for columns 1,2
+for (v in 1: (ncol(means_stds)-1))                        # set names for each mean/std column
+	colnames(act_subj_avg)[v+2] = paste("mean(", colnames(means_stds)[v+1], ")", sep="")
 
 for (a in 1: length(act_label_names)) # activity
 	for (s in 1: no_subjects)         # subject
 	{
-		act_subj_avg[(a-1) * no_subjects + s, 1] <- a # column 1: activity
-		act_subj_avg[(a-1) * no_subjects + s, 2] <- s # column 2: subject
+		act_subj_avg[(a-1) * no_subjects + s, 1] <- act_label_names[a] # column 1: activity
+		act_subj_avg[(a-1) * no_subjects + s, 2] <- s                  # column 2: subject
 	}
 
-# binary:
+# binary - also save levels:
 # save(act_subj_avg, file="act_subj_avg.RData")
 # load("act_subj_avg.RData")
 
 # text:
 # write.table(act_subj_avg, file="act_subj_avg.txt")
 # act_subj_avg <- read.table("act_subj_avg.txt")
-
